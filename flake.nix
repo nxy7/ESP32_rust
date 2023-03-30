b4c06a91b743affc757af54a5c8473206022c02a
nxyt <lolnoxy@gmail.com>
2023-03-31 00:18:19 +0200

    nix devshell added

--- flake.nix
+++ flake.nix
@@ -1,63 +1,18 @@
 {
-  description = "ESP8266/ESP32 development tools";
+  description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    esp-env.url = "github:mirrexagon/nixpkgs-esp-dev";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    {
-      overlay = import ./overlay.nix;
-    } // flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        shell = import ./nix/shell.nix;
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlay ];
-        };
+  outputs = { self, nixpkgs, flake-utils, esp-env }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let pkgs = import nixpkgs { inherit system; };
       in {
-        packages = {
-          inherit (pkgs)
-            gcc-riscv32-esp32c3-elf-bin gcc-xtensa-esp32-elf-bin
-            gcc-xtensa-esp32s2-elf-bin gcc-xtensa-esp32s3-elf-bin
-            openocd-esp32-bin esp-idf
-
-            gcc-xtensa-lx106-elf-bin crosstool-ng-xtensa gcc-xtensa-lx106-elf;
-        };
-
-        devShells = rec {
-          # esp32c3-idf = import ./shells/esp32c3-idfxx.nix { inherit pkgs; };
-          esp32s2-idf = pkgs.mkShell {
-            name = "esp-idf";
-
-            buildInputs = with pkgs; [
-              gcc-xtensa-esp32-elf-bin
-              openocd-esp32-bin
-              esp-idf
-              esptool
-
-              # Tools required to use ESP-IDF.
-              git
-              wget
-              gnumake
-
-              flex
-              bison
-              gperf
-              pkgconfig
-
-              cmake
-              ninja
-
-              ncurses5
-            ];
-          };
-
-          # esp32s3-idf = import ./shells/esp32s3-idf.nix { inherit pkgs; };
-          # esp32-idf = import ./shells/esp32-idf.nix { inherit pkgs; };
-          # esp8266 = import ./shells/esp8266.nix { inherit pkgs; };
-          default = esp32s2-idf;
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.hello ];
+          inputsFrom = [ esp-env.devShells.${system}.esp32s2-idf ];
         };
       });
 }
